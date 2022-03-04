94017a276294e68426c7fe3570b5333bcadd466a
Redkeytf <kmjkds@naver.com>
2022-03-04 16:26:24 +0900

    연습

---
+++ src/pages/home/Home.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Header from '../../header/Header'
+import "./home.css"
+
+export default function Home() {
+  return (
+    <>
+    <Header/>
+    <div className="home">
+        
+        Homepage
+        </div>
+        </>
+  )
+}
